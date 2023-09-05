f7e4c8c1aa15317968bc831d3d7dd46d6b889ce1
Walter Zein <wfzein@gmail.com>
2023-09-04 21:05:46 -0300

    Corrected product.model.js to point to latest products collection

--- src/dao/models/product.model.js
+++ src/dao/models/product.model.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 import mongoosePaginate from "mongoose-paginate-v2"
 
-const prodCollection = "newproducts"
+const prodCollection = "products"
 
 const prodSchema = new mongoose.Schema({
   code: String,
